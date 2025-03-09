ebfcb7821101a995581b47a4f8a1903c416eb0aa
Вова <gorpinichvova2007@gmail.com>
2025-03-09 11:24:20 +0200

    Обновлены файлы App.jsx и main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const fruits = ["яблоко", "банан", "апельсин"]; // начальный массив
+
+//1) добавить грушу в конец списка и ананас в начало
+fruits.push = ["груша"];
+fruits.unshift = ["ананас"];
+console.log(fruits);
+
+//2) убрать яблоко из массива
+const fruits = ["ананас", "яблоко", "банан", "апельсин", "груша"];
+const decremention = fruits.splice(1, 1);
+console.log(decremention);
+
+//3) отсортировать по алфавиту
+const sortedFruits = ["ананас","банан", "апельсин", "груша"].sort();
+console.log(sortedFruits);
